4c36bd51a7521e8ca4be557d67836d39d5378f06
aisleenm18 <Marcus-A2@ulster.ac.uk>
2025-09-28 21:48:05 +0100

    Updated program code with if-else statements.

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,9 +1,44 @@
-﻿Console.WriteLine("Type in the first number followed by the Enter key");
-int firstNumber = Convert.ToInt32(Console.ReadLine());
+﻿CalculatorApp();
+void CalculatorApp()
+{
+    int firstNumber = 0;
+    int secondNumber = 0;
+    int result = 0;
+    int choice = 0;
 
-Console.WriteLine("Type in the second number followed by the Enter key");
-int secondNumber = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Type in the first number followed by the Enter key");
+    firstNumber = Convert.ToInt32(Console.ReadLine());
 
-int result = firstNumber + secondNumber;
+    Console.WriteLine("Type in the second number followed by the Enter key");
+    secondNumber = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Adding {0} and {1} give the amswer {2}", firstNumber, secondNumber, result);
+    Console.WriteLine("Choose an option from the following list: ");
+    Console.WriteLine("1 - Add");
+    Console.WriteLine("2 - Subtract");
+    Console.WriteLine("3 - Divide");
+    Console.WriteLine("4 - Multiply");
+
+    choice = Convert.ToInt32(Console.ReadLine());
+    if (choice == 1)
+        result = firstNumber + secondNumber;
+    else if (choice == 2)
+    {
+        result = firstNumber - secondNumber;
+        Console.WriteLine($"Subtracting {firstNumber} from {secondNumber} equals {result}");
+    }
+    else if (choice == 3)
+    {
+        result = firstNumber / secondNumber;
+        Console.WriteLine($"Dividing {firstNumber} by {secondNumber} equals {result}");
+    }
+    else if (choice == 4)
+    {
+        result = firstNumber * secondNumber;
+        Console.WriteLine($"Multiplying {firstNumber} by {secondNumber} equals {result}");
+    }
+    else
+    {
+        Console.WriteLine("You did not select a valid number between 1-4");
+    }
+        Console.WriteLine($"Adding {0} and {1} equals {2}", firstNumber, secondNumber, result);
+}
